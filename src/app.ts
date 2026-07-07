b81a36b79590c21f8dcaf1df2d5dad942cfae7eb
zayedutsho <zayedutsho@gmail.com>
2026-07-07 20:05:48 +0600

    backend project setup done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express, { Application, Request, Response } from "express";
+import config from "./config";
 
 const app: Application = express();
+
+//middleware
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello GearUp server");
