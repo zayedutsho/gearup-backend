9be386d3a73b0cfae9c142d231ade18512b9cd51
zayedutsho <zayedutsho@gmail.com>
2026-07-09 01:46:48 +0600

    provider validation done

---
+++ src/modules/provider/provider.service.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const updateRentalStatusSchema = z.object({
+  status: z.enum(["CONFIRMED", "PICKED_UP", "RETURNED", "CANCELLED"]),
+});
+
+export const providerValidation = {
+  updateRentalStatusSchema,
+};
